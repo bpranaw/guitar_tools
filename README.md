2535d1713ebd322149c158628bb4699c73993cba
Pranaw <bpranaw@gmail.com>
2023-05-22 16:01:25 -0700

    Setting up a basic window.

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
-# Guitar Tools 
+# Guitar Tools
 
 # How to run
 
+If on linux:
+
+run:
+sudo apt-get install libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libxkbcommon-dev libssl-dev
+
+For eframe to work.
+
 # Progress
 
-
-
+LOG 1: Been researching and deciding on what GUI technology to use. Settled on egui and its supporting eframe. I have managed to set up a basic window so far.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use eframe::egui;
+
 fn main() {
+    // Taken from eframe documentation: https://docs.rs/eframe/latest/eframe/
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native(
+        "Guitar Tools",
+        native_options,
+        Box::new(|cc| Box::new(GuitarToolsApp::new(cc))),
+    )
+    .expect("Something went wrong with the app...");
+
     println!("Hello, world!");
 }
+
+// Taken from eframe documentation: https://docs.rs/eframe/latest/eframe/
+#[derive(Default)]
+struct GuitarToolsApp {}
+
+impl GuitarToolsApp {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for GuitarToolsApp {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello World!");
+        });
+    }
+}
+
+/*
+   Purpose:
+   Notes:
+*/
