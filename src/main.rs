558f88ab39f3a366345b022ef23d21081355d733
Pranaw <bpranaw@gmail.com>
2023-06-04 21:35:02 -0700

    Got buttons to a workable stage

--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,33 @@
 use eframe::egui;
 
 // MAIN -------------------------------------------------------------------------------------------------------------------------
-fn main() {
+fn main() -> Result<(), eframe::Error> {
     // Taken from eframe documentation: https://docs.rs/eframe/latest/eframe/
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "Guitar Tools",
         native_options,
-        Box::new(|cc| Box::new(GuitarToolsApp::new(cc))),
+        Box::new(|_cc| Box::<GuitarToolsApp>::default()),
     )
-    .expect("Something went wrong with the app...");
-
-    println!("Hello, world!");
 }
 
 // UI ---------------------------------------------------------------------------------------------------------------------------
 // Adapted from eframe documentation: https://docs.rs/eframe/latest/eframe/
-#[derive(Default)]
-struct GuitarToolsApp {}
+struct GuitarToolsApp {
+    app_mode: AppModeOptions,
+}
 
-impl GuitarToolsApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-        Self::default()
+impl Default for GuitarToolsApp {
+    fn default() -> Self {
+        Self {
+            app_mode: AppModeOptions::Home,
+        }
     }
 }
 
 impl eframe::App for GuitarToolsApp {
-    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            draw_panel(ctx);
-        });
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        draw_panel(&mut self.app_mode, ctx);
     }
 }
 
@@ -41,16 +35,15 @@ impl eframe::App for GuitarToolsApp {
    Purpose:
    Notes:
 */
-fn draw_panel(ctx: &egui::Context) {
-    let mut app_mode = AppModeOptions::Home;
+fn draw_panel(app_mode: &mut AppModeOptions, ctx: &egui::Context) {
     egui::CentralPanel::default().show(ctx, |ui| {
         ui.heading("Hello there!");
         ui.end_row();
         ui.horizontal(|ui| {
-            ui.radio_value(&mut app_mode, AppModeOptions::Home, "Home");
-            ui.radio_value(&mut app_mode, AppModeOptions::TuneByEar, "Tune by ear");
+            ui.radio_value(app_mode, AppModeOptions::Home, "Home");
+            ui.radio_value(app_mode, AppModeOptions::TuneByEar, "Tune by ear");
             ui.radio_value(
-                &mut app_mode,
+                app_mode,
                 AppModeOptions::TuneByRecording,
                 "Tune by recording",
             );
